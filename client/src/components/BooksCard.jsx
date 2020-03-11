18ed41e6f83fd86a8bdb57eb224bdd44ff40f0e7
cferdinand <chrisferdinand07@gmail.com>
2020-03-11 12:07:00 -0400

    added ellipsis. Finised all styling

--- client/src/components/BooksCard.jsx
+++ client/src/components/BooksCard.jsx
@@ -14,19 +14,22 @@ const BooksCard = ({ book }) => {
 
   return (
     <div className="book-wrapper">
-      <div className="transparent">
-        <img className="book-photo" src={book.photo} />
+      <div className="transparent book-view">
+        <img className="book-photo book-view" src={book.photo} />
         <Summary desc={book.book_description} />
       </div>
-      <div className="book-content">
-        <div className="book-info">
-          <div className="name-author">
-            <p>{book.book_name}</p>
-            <p>
-              by <span>{book.author}</span>
-            </p>
+      <div className="book-content book-view">
+        <div className="book-menu dotted-menu book-view">
+          <li></li>
+          <li></li>
+          <li></li>
+        </div>
+        <div className="book-info book-view">
+          <div className="name-author book-view">
+            <p className="book-name book-view">{book.book_name}</p>
+            <p className="book-author book-view">{`by ${book.author}`}</p>
           </div>
-          <div className="book-meta">
+          <div className="book-meta book-view">
             <Stars
               value={Number(book.ratings)}
               name="half-rating"
@@ -34,16 +37,18 @@ const BooksCard = ({ book }) => {
               size="small"
               readOnly={true}
             />
-            <span>{`${Number(book.votes).toLocaleString()} voters`}</span>
+            <span className="book-rating book-view">{`${Number(
+              book.votes
+            ).toLocaleString()} voters`}</span>
           </div>
           <div className="description">
             <p>{book.book_description.slice(0, 100)}</p>
           </div>
         </div>
       </div>
-      <div className="book-likes">
-        <hr className="likes-hr" />
-        <div className="liker">
+      <div className="book-likes book-view">
+        <hr className="likes-hr book-view" />
+        <div className="liker book-view">
           <ListOfLikes />
           <Likers book={book} />
         </div>
